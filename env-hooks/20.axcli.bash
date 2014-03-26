562885cfd173f20158402df47e198e011501d57c
Paul Mathieu <paul.mathieu@pal-robotics.com>
2014-03-26 12:47:53 +0100

    Allow optional arguments anywhere in the command
    This fixes the tab completion of the action name and goal contents
    by ignoring any argument beginning with a '-'
    
    Fixes #2

--- env-hooks/20.axcli.bash
+++ env-hooks/20.axcli.bash
@@ -3,12 +3,19 @@ function _roscomplete_axcli
     local arg opts
     COMPREPLY=()
     arg="${COMP_WORDS[COMP_CWORD]}"
+    local cword=$COMP_CWORD
+    for a in $(seq $((COMP_CWORD-1))); do
+        if [ -z "${COMP_WORDS[a]//-*}" ]; then
+            ((cword--))
+        fi
+    done
+    local words=(${COMP_WORDS[@]//-*})
 
-    if [[ $COMP_CWORD == 1 ]]; then
+    if [[ $cword == 1 ]]; then
         opts=`rostopic list | grep '/goal$' | sed 's,/goal$,,' 2> /dev/null`
         COMPREPLY=($(compgen -W "$opts" -- ${arg}))
-    elif [[ $COMP_CWORD == 2 ]]; then
-        mtype=`rostopic type ${COMP_WORDS[1]}/goal`
+    elif [[ $cword == 2 ]]; then
+        mtype=`rostopic type ${words[1]}/goal`
         opts=`rosmsg-proto msg 2> /dev/null -s ${mtype:0:-10}Goal`
         if [ 0 -eq $? ]; then
             COMPREPLY="$opts"
